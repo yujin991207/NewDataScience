928450fa989adb508ace38eefb4e9cadde59a813
study2 <mnmn0905@naver.com>
2022-11-17 18:40:38 +0900

    11월 17일 학원에서 추가한 파일2

---
+++ PythonBasic/Chapter06/170p.py
@@ -0,0 +1,39 @@
+# 다형성
+# 부모클래스
+class Flight:
+    # 부모 원형 함수
+    def fly(self):
+        print('날다, fly 원형 메서드')
+
+
+# 자식클래스 : 비행기
+class Airplane(Flight):
+    def fly(self):
+        print('비행기가 날다.')
+
+# 자식클래스 : 새
+class Bird(Flight):
+    def fly(self):
+        print('새가 날다.')
+
+# 자식클래스 : 종이비행기
+class paperAirplane(Flight):
+    def fly(self):
+        print('종이비행기가 날다.')
+
+# 객체생성
+flight = Flight() # 부모클래스 객체
+air = Airplane() # 자식1클래스 객체
+bird = Bird() # 자식2클래스 객체
+paper = paperAirplane() # 자식3클래스 객체
+
+flight.fly()
+
+flight = air
+flight.fly()
+
+flight = bird
+flight.fly()
+
+flight = paper
+flight.fly()
