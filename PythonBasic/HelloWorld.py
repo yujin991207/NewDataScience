32815caa287f261c8688ffe054e413b74740a534
study2 <mnmn0905@naver.com>
2022-11-23 18:19:55 +0900

    11월 23일 학원에서 추가한 파일

---
+++ PythonBasic/HelloWorld.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("프로그램 실행 준비")
+
+
+# In[24]:
+
+
+print("Jupyter 테스트1")
+
+
+# In[25]:
+
+
+# A키는 현재 셀의 위에서 새로운 셀을 만든다
+print("Hello World")
+num1 = int(input("첫번째 수를 입력하세요"))
+
+print(num1)
+# B키는 현재 셀의 아래에서 새로운 셀을 만든다
+
+
+# In[26]:
+
+
+num1
+
+
+# In[27]:
+
+
+type(num1)
+
+
+# In[28]:
+
+
+num2 = int(input("두번째 수를 입력하세요"))
+
+
+# In[29]:
+
+
+num2
+
+
+# In[30]:
+
+
+num1 + num2
+
+
+# In[23]:
+
+
+print("Jupyter 테스트1")
+
+# A키는 현재 셀의 위에서 새로운 셀을 만든다
+print("Hello World")
+num1 = int(input("첫번째 수를 입력하세요"))
+
+print(num1)
+# B키는 현재 셀의 아래에서 새로운 셀을 만든다
+num2 = int(input("두번째 수를 입력하세요"))
+
+num1 + num2
+
