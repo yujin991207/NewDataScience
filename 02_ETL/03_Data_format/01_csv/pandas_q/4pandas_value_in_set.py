5657c63b39a788c8fe9fdbfdd0e218149e146345
yujin991207 <yujin991207@naver.com>
2022-12-15 20:54:54 +0900

    12월 15일 노트북 추가 파일

--- 02_ETL/03_Data_format/01_csv/pandas_q/4pandas_value_in_set.py
+++ 02_ETL/03_Data_format/01_csv/pandas_q/4pandas_value_in_set.py
@@ -2,13 +2,13 @@
 import pandas as pd
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2] # output_files/4output_pandas.csv
+#input_file = 'supplier_data.csv'
+output_file = 'output_files/4output_pandas.csv'
 
-data_frame = pd.read_csv(input_file)
+#data_frame = pd.read_csv(input_file)
 
 important_dates = ['1/20/14', '1/30/14']
-data_frame_value_in_set = data_frame.loc[data_frame['Purchase Date']\
-.isin(important_dates), :]
+#data_frame_value_in_set = data_frame.loc[data_frame['Purchase Date']\
+#.isin(important_dates), :]
 
-data_frame_value_in_set.to_csv(output_file, index=False)
+#data_frame_value_in_set.to_csv(output_file, index=False)
